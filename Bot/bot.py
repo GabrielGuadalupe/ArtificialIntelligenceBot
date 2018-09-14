d7d10398df6db27f4f0c59daee6c64dd1e134e6f
Satan <satan@localhost.localdomain>
2018-09-14 10:01:44 -0700

    Made some changes to the 'voice'

--- Bot/bot.py
+++ Bot/bot.py
@@ -6,7 +6,6 @@ kernel = aiml.Kernel()
 
 engine = pyttsx.init()
 
-
 if os.path.isfile("bot_brain.brn"):
     kernel.bootstrap(brainFile = "bot_brain.brn")
 else:
@@ -15,7 +14,8 @@ else:
 
 # Kernel now ready for use
 while True:
-    response = (kernel.respond(input(">> ")))
+    engine.setProperty('rate', 125)
+    response = (kernel.respond(raw_input(">> ")))
     engine.say(response)
     print(response)
     engine.runAndWait()
